5e50dfdfeedc638119767a51d7405e27fb149d13
dk720 <dk720@LAPTOP-T8BEJAIO>
2021-06-21 23:56:25 +0900

    NoticeListAction페이지 만들어놓음 view -> action 페이지로 코드 이동하려고

---
+++ src/com/deco/notice/action/NoticeListAction.java
@@ -0,0 +1,20 @@
+package com.deco.notice.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NoticeListAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		
+		/** action페이지에서 정보 넘김
+		req.setAttribute("shareList", shareList);
+		req.setAttribute("pageNum", pageNum);
+		req.setAttribute("pageSize", pageSize);*/
+		
+		return null;
+	}
+
+}
