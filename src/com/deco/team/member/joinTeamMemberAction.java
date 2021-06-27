9d1c5ad8b08e30066ba9937a9b50007f3438bdeb
Leedohyun <ehgus9443@naver.com>
2021-06-27 19:57:46 +0900

    join member 페이지 이동

--- src/com/deco/team/member/joinTeamMemberAction.java
+++ src/com/deco/team/member/joinTeamMemberAction.java
@@ -1,11 +1,14 @@
 package com.deco.team.member;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.deco.Action;
 import com.deco.ActionForward;
+import com.deco.team.teamDAO;
 
 public class joinTeamMemberAction implements Action {
 
@@ -28,20 +31,38 @@ public class joinTeamMemberAction implements Action {
 		
 		teamMemberDTO tmDTO = new teamMemberDTO();
 		
-		tmDTO.setTeam_idx(Integer.parseInt(req.getParameter("team_idx")));
+		int team_idx = Integer.parseInt(req.getParameter("team_idx"));
+		
+		tmDTO.setTeam_idx(team_idx);
 		tmDTO.setMember(user_num);
 		
 		teamMemberDAO tmDAO = new teamMemberDAO();
 		
 		int result = tmDAO.checkSubmitMember(Integer.parseInt(req.getParameter("team_idx")));
 		
+		resp.setContentType("text/html; charset=utf-8");
+		PrintWriter out = resp.getWriter();
 		
-		
-		tmDAO.joinTeam(tmDTO);
-		
-		forward = new ActionForward("./teamMain.te", true);
-		
-		return forward;
+		if(result < new teamDAO().getLimitPersonOfTeamIdx(team_idx)){
+			tmDAO.joinTeam(tmDTO);
+			
+			out.print("<script>");
+			out.print("alert('신청이 완료 되었습니다.');");
+			out.print("location.href='./teamMain.te';");
+			out.print("</script>");
+			out.close();
+			
+			return null;
+			
+		} else {
+			out.print("<script>");
+			out.print("alert('더이상 신청을 하실 수 없습니다.');");
+			out.print("history.back();");
+			out.print("</script>");
+			out.close();
+			
+			return null;
+		}
 	}
 
 }
